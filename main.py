d7bc3d2e8939e60efd478a3b7d2ada6701a4bdae
SGWinter <seangcalgary@gmail.com>
2024-08-29 09:39:09 -0600

    initial game loop setup

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import pygame
 from constants import *
+import os
+os.environ['SDL_AUDIODRIVER'] = 'dsp'
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
